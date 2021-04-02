038961a7dcac35ee853eb2b50b001aa3faed2471
linux_china <libing.chen@gmail.com>
2021-04-02 12:09:37 -0700

    format

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 addEventListener("fetch", (event: Event) => {
-    const response = new Response("Hello World!", {
-        headers: {"content-type": "text/plain"},
-    });
-    (event as FetchEvent).respondWith(response);
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  (event as FetchEvent).respondWith(response);
 });
