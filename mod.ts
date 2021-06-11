c91db133059c284b24f3f2e13d4b5c9750658584
linux_china <libing.chen@gmail.com>
2021-06-10 23:26:26 -0700

    Polishing

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 const handler = (event: FetchEvent) => {
-    const response = new Response("Hello Leijuan!!", {
+    const response = new Response("Hello Deno!!", {
         headers: {"content-type": "text/plain"},
     });
     event.respondWith(response);
